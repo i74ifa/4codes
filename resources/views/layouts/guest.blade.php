a6d08da27b6d687440393e0e16eee4932a703d1e
i74ifa <i74ifa@gmail.com>
2021-11-13 14:49:04 +0300

    add user logout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script defer src="{{ asset('js/app.js') }}"></script>
     {{-- @paddleJS --}}
 </head>
 

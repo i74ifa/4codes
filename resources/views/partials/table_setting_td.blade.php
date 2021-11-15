4325fcdb34edee64f1b22844a954a63f7f464fd2
i74ifa <i74ifa@gmail.com>
2021-11-15 08:38:03 +0300

    edit paginate style

--- resources/views/partials/table_setting_td.blade.php
+++
@@ -1,3 +0,0 @@
-<td class=" py-4 whitespace-nowrap text-right text-sm font-medium">
-    <a href="#" class="text-indigo-600 hover:text-indigo-900"><span class="eva eva-trash text-lg"></span></a>
-</td>

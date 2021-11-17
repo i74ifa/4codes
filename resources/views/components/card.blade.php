512fbee9bf74375d1b1ae4111ce149b89d91d434
i74ifa <i74ifa@gmail.com>
2021-11-17 23:42:03 +0300

    update design

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -2,10 +2,12 @@
 <div
     class="my-1 px-1 w-full sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/2 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4">
     <div class="w-full justify-center items-center bg-white shadow hover:shadow-md duration-500 flex flex-col border">
-        <img src="{{ $src }}" alt="img" title="img" class="w-full h-auto object-cover">
+        <a href="{{ $cardDetail }}">
+            <img src="{{ $src }}" alt="img" title="img" class="w-full h-auto object-cover">
+        </a>
         <div class="w-full pt-5 px-2 py-3 justify-start flex flex-col">
             <div class="overflow-hidden">
-                <h4 class="py-4">{{ $cardTitle }}</h4>
+                <h4 class="py-4 text-sm">{{ $cardTitle }}</h4>
             </div>
             <hr>
             <div class="flex justify-between">
